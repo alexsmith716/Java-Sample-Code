d46e1192a9de8b164f411e05d1f09f42e36e0531
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-07-25 14:44:59 -0400

    feat: Added new code sample Lab-Datatypes.

---
+++ Lab-Datatypes/VariableDeclaration.java
@@ -0,0 +1,26 @@
+public class VariableDeclaration {
+	public static void main(String[] args) {
+		System.out.println("Displaying Simple Data Types");
+		boolean on = false;
+		System.out.println(on);
+		short s = 16;
+		System.out.println(s);
+		int i = -32;
+		System.out.println(i);
+		float f = 3839.34839f;
+		System.out.println(f);
+		long l = 4294967296l;
+		System.out.println(l);
+		char c = 'a';
+		System.out.println(c);
+	}
+}
+
+//	% java VariableDeclaration
+//	Displaying Simple Data Types
+//	false
+//	16
+//	-32
+//	3839.3484
+//	4294967296
+//	a
