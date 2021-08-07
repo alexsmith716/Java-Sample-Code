de14fc576deb8538a2e6bd9560ea402ecd8a05df
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-08-07 13:41:35 -0400

    feat: Added new code samples Lab-StaticInstance and Lab-Abstract and Keyword-Abstract.

---
+++ 14-More-Keywords/Lab-Abstract/src/com/alex/test/TestPerson.java
@@ -0,0 +1,12 @@
+package com.alex.test;
+
+import com.alex.Person;
+
+public class TestPerson {
+	public static void main(String[] args) {
+
+		// Person cannot be instantiated
+		// You cannot instantiate an abstract class.
+		Person adam = new Person();
+	}
+}
